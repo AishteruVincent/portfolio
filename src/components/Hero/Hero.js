ecdae6eeb952bc54390b5b2fcc4fa1c0c0746d07
Vincent Rafael Cunanan <vincentcunanan53@gmail.com>
2024-05-14 00:01:55 +0800

    Changes to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,10 +11,13 @@ const Hero = (props) => (
       Welcome to <br/>
       My Personal Portfolio
     </SectionTitle>
-    <SectionText> I want to be a part of a progressive and highly dynamic
-company and to excel in all of the company's activities while
-also continuing to learn and grow my knowledge, abilities,
-and experience.
+    <SectionText> Enthusiastic and motivated individual eager to kickstart a
+career in mobile development, with a particular focus on
+Flutter technology. Possesses a strong foundation in
+software development principles and a keen interest in
+learning and mastering Flutter for creating innovative
+mobile applications.
+
     </SectionText>
     <Button onClick={() => window.location = "https://github.com/AishteruVincent"}>Learn More</Button>
   </LeftSection>
